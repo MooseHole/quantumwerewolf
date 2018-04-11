110b21de51dddae7bb4249577a55e0e36c16e1d7
Mike Schreiber <Mike.Schreiber@VGT.net>
2018-04-11 13:52:19 -0500

    Debugging redisplay rows closed

--- internal/app/quantumwerewolf/game.go
+++ internal/app/quantumwerewolf/game.go
@@ -45,36 +45,39 @@ func rebuildGame(c *gin.Context, gameID int) {
 	gameQuery += " LIMIT 1"
 
 	row, err := db.Query(gameQuery)
-	if quantumutilities.HandleErr(c, err, "Error selecting game") {
+	if quantumutilities.HandleErr(c, err, "Error selecting game ["+gameQuery+"]") {
 		return
 	}
 
-	row.Next()
-	rolesByteArray := make([]byte, 0, 100)
-	err = row.Scan(&game.Number, &gameSetup.Name, &gameSetup.Total, &rolesByteArray, &gameSetup.Keep, &game.RoundNum, &game.RoundNight, &game.Seed)
+	if row.Next() {
+		rolesByteArray := make([]byte, 0, 100)
+		err = row.Scan(&game.Number, &gameSetup.Name, &gameSetup.Total, &rolesByteArray, &gameSetup.Keep, &game.RoundNum, &game.RoundNight, &game.Seed)
+		row.Close()
+
+		if quantumutilities.HandleErr(c, err, "Error scanning game variables ["+gameQuery+"]") {
+			return
+		}
+
+		err = quantumutilities.GetInterface(rolesByteArray, &gameSetup.Roles)
+		if quantumutilities.HandleErr(c, err, "Error getting game roles interface ["+gameQuery+"]") {
+			return
+		}
+	}
 	row.Close()
-	if quantumutilities.HandleErr(c, err, "Error scanning game variables") {
-		return
-	}
-
-	err = quantumutilities.GetInterface(rolesByteArray, &gameSetup.Roles)
-	if quantumutilities.HandleErr(c, err, "Error getting game roles interface") {
-		return
-	}
 
 	playerQuery := "SELECT name, num, actions FROM players"
 	playerQuery += " WHERE gameid=" + strconv.Itoa(gameID)
 	playerQuery += " LIMIT " + strconv.Itoa(gameSetup.Total)
 
 	row, err = db.Query(playerQuery)
-	if quantumutilities.HandleErr(c, err, "Error selecting players") {
+	if quantumutilities.HandleErr(c, err, "Error selecting players ["+playerQuery+"]") {
 		return
 	}
 
 	for row.Next() {
 		var player Player
 		err = row.Scan(&player.Name, &player.Num, &player.Actions)
-		if quantumutilities.HandleErr(c, err, "Error scanning player variables") {
+		if quantumutilities.HandleErr(c, err, "Error scanning player variables ["+playerQuery+"]") {
 			return
 		}
 		players = append(players, player)
